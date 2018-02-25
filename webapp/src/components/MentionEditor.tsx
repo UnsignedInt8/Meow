330389f0d5271ab2f7f5cdbbc1f8f4b05e92e179
UnsignedInt8 <unsignedint8@UnsignedInt8s-MacBook-Pro.local>
2018-02-25 09:48:17 +0800

    add emoji

--- webapp/src/components/MentionEditor.tsx
+++ webapp/src/components/MentionEditor.tsx
@@ -2,58 +2,69 @@ import React, { Component } from 'react';
 import { EditorState } from 'draft-js';
 import Editor from 'draft-js-plugins-editor';
 import createMentionPlugin, { defaultSuggestionsFilter } from 'draft-js-mention-plugin';
+import createEmojiPlugin from 'draft-js-emoji-plugin';
+import 'draft-js-emoji-plugin/lib/plugin.css';
 import 'draft-js-mention-plugin/lib/plugin.css';
 import './styles/editor.css';
 import './styles/mentions.css';
+import './styles/emoji.css';
 
 import mentions from './mentions';
 
 const Entry = (props) => {
     const {
-      mention,
-      searchValue, // eslint-disable-line no-unused-vars
-      isFocused, // eslint-disable-line no-unused-vars
-      ...parentProps
+        mention,
+        searchValue, // eslint-disable-line no-unused-vars
+        isFocused, // eslint-disable-line no-unused-vars
+        ...parentProps
     } = props;
-  
+
     return (
-      <div {...parentProps}>
-        <div className='mentionSuggestionsEntryContainer'>
-          <div className='mentionSuggestionsEntryContainerLeft'>
-            <img
-              src={mention.get('avatar')}
-              className='mentionSuggestionsEntryAvatar'
-              role="presentation"
-            />
-          </div>
-  
-          <div className='mentionSuggestionsEntryContainerRight'>
-            <div className='mentionSuggestionsEntryText'>
-              {mention.get('name')}
+        <div {...parentProps}>
+            <div className='mentionSuggestionsEntryContainer'>
+                <div className='mentionSuggestionsEntryContainerLeft'>
+                    <img
+                        src={mention.get('avatar')}
+                        className='mentionSuggestionsEntryAvatar'
+                        role="presentation"
+                    />
+                </div>
+
+                <div className='mentionSuggestionsEntryContainerRight'>
+                    <div className='mentionSuggestionsEntryText'>
+                        {mention.get('name')}
+                    </div>
+
+                    <div className='mentionSuggestionsEntryTitle'>
+                        {mention.get('title')}
+                    </div>
+                </div>
             </div>
-  
-            <div className='mentionSuggestionsEntryTitle'>
-              {mention.get('title')}
-            </div>
-          </div>
         </div>
-      </div>
     );
-  };
-  
+};
+
 
 export default class MentionEditor extends Component {
 
     mentionPlugin: any;
+    emojiPlugin: any;
     editor: Editor;
+    plugins: any[] = [];
 
     constructor(props) {
         super(props);
 
+        this.emojiPlugin = createEmojiPlugin({
+            selectButtonContent: '☻',
+            useNativeArt: false,
+        });
         this.mentionPlugin = createMentionPlugin({
             entityMutability: 'IMMUTABLE',
             mentionPrefix: '@',
         });
+
+        this.plugins = [this.mentionPlugin, this.emojiPlugin];
     }
 
     state = {
@@ -66,6 +77,8 @@ export default class MentionEditor extends Component {
         this.setState({
             editorState,
         });
+        let blocks = editorState.getCurrentContent().getBlocksAsArray();
+        blocks.forEach(b => console.log(b.toString()));
     };
 
     onSearchChange = ({ value }) => {
@@ -87,22 +100,28 @@ export default class MentionEditor extends Component {
 
     render() {
         const { MentionSuggestions } = this.mentionPlugin;
-        const plugins = [this.mentionPlugin];
+        const { EmojiSuggestions, EmojiSelect } = this.emojiPlugin;
 
         return (
-            <div className={`editor ${this.state.isFocused ? 'editor_focused' : ''}`} onBlur={this.blur} onClick={this.focus}>
-                <Editor
-                    editorState={this.state.editorState}
-                    onChange={this.onChange}
-                    plugins={plugins}
-                    ref={(element) => { this.editor = element; }}
-                />
-                <MentionSuggestions
-                    onSearchChange={this.onSearchChange}
-                    suggestions={this.state.suggestions}
-                    onAddMention={this.onAddMention}
-                    entryComponent={Entry}
-                />
+            <div>
+                <div className={`editor ${this.state.isFocused ? 'editor_focused' : ''}`} onBlur={this.blur} onClick={this.focus}>
+                    <Editor
+                        editorState={this.state.editorState}
+                        onChange={this.onChange}
+                        plugins={this.plugins}
+                        ref={(element) => { this.editor = element; }}
+                    />
+                    <MentionSuggestions
+                        onSearchChange={this.onSearchChange}
+                        suggestions={this.state.suggestions}
+                        onAddMention={this.onAddMention}
+                        entryComponent={Entry}
+                    />
+                    <EmojiSuggestions />
+                </div>
+                <div className='editor_options'>
+                    <EmojiSelect />
+                </div>
             </div>
         );
     }
